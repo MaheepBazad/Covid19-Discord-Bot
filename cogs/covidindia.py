3ceabb1abe81e547df18a5c2598dbd55f5194272
MaheepBazad <105652082+MaheepBazad@users.noreply.github.com>
2022-06-02 11:29:17 +0530

    Update covidindia.py

--- cogs/covidindia.py
+++ cogs/covidindia.py
@@ -1,7 +1,7 @@
 import discord
 import requests
 from discord.ext import commands
-
+from quickchart import QuickChart
 
 class india(commands.Cog):
     def __init__(self, bot):
@@ -18,13 +18,30 @@ class india(commands.Cog):
 
                 headers = {
                     "X-RapidAPI-Host": "covid-19-india2.p.rapidapi.com",
-                    "X-RapidAPI-Key": "KEY",
+                    "X-RapidAPI-Key": "07e8df961bmsh592a2dd1213373fp11a2e7jsn369e6d4970ea",
                 }
                 data = requests.get(url, headers=headers).json()
                 state = data[f'{state}']["state"]
                 totalCases = data[f'{state}']["total"]
                 totalDeaths = data[f'{state}']["death"]
                 recovered = data[f'{state}']["cured"]
+                qc = QuickChart()
+                qc.width = 500
+                qc.height = 300
+                qc.device_pixel_ratio = 2
+                qc.config = {
+                    "type": "bar",
+                    "data": {
+                        "labels": ["Total cases", "Deaths", "Recovered"],
+                        "datasets": [
+                            {
+                                "label": "Graph on COVID19 Data ",
+                                "data": [totalCases, totalDeaths, recovered],
+                            }
+                        ],
+                    },
+                }
+                image = qc.get_url()
                 embed = discord.Embed(
                     title=f"COVID19 data for {state}!",
                     description=f"These are the current stastics of the spread of coronavirus for {state}",
@@ -35,9 +52,9 @@ class india(commands.Cog):
                 embed.add_field(name="Total Cases", value=totalCases, inline=True)
                 embed.add_field(name="Deaths", value=totalDeaths, inline=True)
                 embed.add_field(name="Recovered", value=recovered, inline=True)
-
+                embed.set_image(url=f"{image}")
                 embed.set_thumbnail(
-                    url="https://assets.weforum.org/article/image/Gt3_maI3Pg1p3LCdz686W_z41IEvOy6elJNQmu_oRLc.jpg"
+                    url="https://gumlet.assettype.com/greaterkashmir%2F2022-05%2Ffb121dec-0760-495b-93f2-ddcab7f8c400%2F2402f44620132cab0e9832b0fe454228.jpg?format=auto"
                 )
                 await ctx.send(embed=embed)
 
@@ -47,4 +64,5 @@ class india(commands.Cog):
 
 def setup(bot):
     bot.add_cog(india(bot))
+
 
